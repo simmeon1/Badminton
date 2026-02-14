05d239b04be6b24b357c92ade0fcaa61cce2c356
simmeon1 <simmeonn1@gmail.com>
2026-02-14 19:47:31 +0000

    comments on .env

--- Badminton.FE/dotenv-replace.ts
+++ Badminton.FE/dotenv-replace.ts
@@ -1,11 +1,16 @@
+// The script looks at environment variables from both environment and .env file
+// and ONLY updates the values of the keys in environment.ts if the keys exist in the vars from above.
+// .env file is only useful for development (in fact only the keys matter and not the values)
+// as pipelines will not have an .env file from source control but will have env vars explicitly set up.
+
 import fs from 'node:fs';
 import dotenv from 'dotenv'
 dotenv.config({ path: '../.env' });
 
 const filePath = "./src/environments/environment.ts";
-let envContent = fs.readFileSync(filePath, 'utf-8');
-const matches = envContent.matchAll(/(\w+): '.*?'/g);
+let environmentTsContent = fs.readFileSync(filePath, 'utf-8');
+const matches = environmentTsContent.matchAll(/(\w+): '.*?'/g);
 for (const match of matches) {
-  envContent = envContent.replaceAll(match[0], match[1] + ": '" + process.env[match[1]] + "'");
+  environmentTsContent = environmentTsContent.replaceAll(match[0], match[1] + ": '" + process.env[match[1]] + "'");
 }
-fs.writeFileSync(filePath, envContent);
+fs.writeFileSync(filePath, environmentTsContent);
