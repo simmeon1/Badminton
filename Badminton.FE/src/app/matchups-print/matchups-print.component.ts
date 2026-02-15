7d720e35f948ec55a8385496244ebb702c3f7f18
simmeon1 <simmeonn1@gmail.com>
2026-02-15 23:35:40 +0000

    sorting

--- Badminton.FE/src/app/matchups-print/matchups-print.component.ts
+++ Badminton.FE/src/app/matchups-print/matchups-print.component.ts
@@ -26,8 +26,18 @@ export class MatchupsPrint {
             result.push({
                 courtIndex,
                 matchups: matchupCollection.matchups.map(m => {
-                    const getPairingText = (p: Pairing) => `${p.player1}-${p.player2}`
-                    return `${getPairingText(m.pairing1)} v. ${getPairingText(m.pairing2)}`
+                    const selectedPlayer = this.selectedPlayer() ?? '';
+                    const pairIncludesPlayer = (p: Pairing) => [p.player1, p.player2].includes(selectedPlayer) ? 1 : 0
+                    const pairs = [m.pairing1, m.pairing2].sort((p1, p2) => pairIncludesPlayer(p2) - pairIncludesPlayer(p1))
+
+                    const getPairingText = (p: Pairing) => {
+                        const players = [p.player1, p.player2].sort((p1, p2) => {
+                            const isPlayer = (p: string) => p === selectedPlayer ? 1 : 0
+                            return isPlayer(p2) - isPlayer(p1);
+                        })
+                        return `${players[0]}-${players[1]}`;
+                    }
+                    return `${getPairingText(pairs[0])} v. ${getPairingText(pairs[1])}`
                 })
             });
         }
