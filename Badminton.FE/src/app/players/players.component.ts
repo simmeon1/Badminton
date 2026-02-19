010b41955e33344efafc028941dbb287c07dea28
simmeon1 <simmeonn1@gmail.com>
2026-02-19 18:51:09 +0000

    Revert "refactor form layout and remove unused clipboard functionality"
    This reverts commit 74138f4f0f7d9a1bd4d2f345246832a8d8180a86.

--- Badminton.FE/src/app/players/players.component.ts
+++ Badminton.FE/src/app/players/players.component.ts
@@ -136,6 +136,11 @@ export class Players {
         this.selectedPlayerChanged.emit(this.selectedPlayer() === name ? undefined : name);
     }
 
+    public copyToClipboard() {
+        navigator.clipboard.writeText(this.dataSource().map(row => row.name).join('\n'));
+        this.snackBar.open('Copied', undefined, {duration: 500});
+    }
+
     public syncToForm() {
         const names = this.dataSource().map(row => row.name);
         this.playersReordered.emit(names);
